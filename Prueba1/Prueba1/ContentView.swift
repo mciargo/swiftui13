30cd53b5ba21d46c3f691e4e3c3648a70e5b0e1e
mciargo <63866829+mciargo@users.noreply.github.com>
2020-04-17 15:33:49 -0400

    Prueba de proyecto
    es la primera prueba de proyeyto para poder cargar en github

---
+++ Prueba1/Prueba1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prueba1
+//
+//  Created by Mauro Ciargo on 4/17/20.
+//  Copyright © 2020 Mauro Ciargo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
