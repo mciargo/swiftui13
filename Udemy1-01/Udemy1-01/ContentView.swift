d995fe16c93bd1c2da5c93b36f53f9e51ffab43c
mciargo <63866829+mciargo@users.noreply.github.com>
2020-04-18 11:25:54 -0400

    cambio de fuente

--- Udemy1-01/Udemy1-01/ContentView.swift
+++ Udemy1-01/Udemy1-01/ContentView.swift
@@ -5,15 +5,25 @@
 //  Created by Mauro Ciargo on 4/18/20.
 //  Copyright © 2020 Mauro Ciargo. All rights reserved.
 //
+// para bajar fuentes ir a dafont.com
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Buongiorno Mondo")
-            .fontWeight(.heavy)
-            .foregroundColor(.blue)
-            .font(.title)
+        VStack {
+             Spacer()
+            Text("Buongiorno Mondo")
+                .fontWeight(.heavy)
+                .foregroundColor(.blue)
+                .font(.title)
+            Spacer()
+            Text("hellooooooo")
+            .fontWeight(.bold)
+            .foregroundColor(.green)
+            .font(.custom("Avenir", size: 18))
+             Spacer()
+        }
     }
 }
 
